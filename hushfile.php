91c2672a334a1e8c5b0e5cf86e1710b45e8bf798
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-04-17 04:07:01 +0200

    working on server

--- hushfile.php
+++ hushfile.php
@@ -26,14 +26,20 @@ if($_SERVER["REQUEST_URI"] != "/") {
 	$cryptofile = $datapath.$fileid."/cryptofile.dat";
 	$metadatafile = $datapath.$fileid."/metadata.dat";
 	
+	// create folder for this file
+	mkdir($datapath.$fileid);
+	
+	// write encrypted file
 	$fh = fopen($cryptofile, 'w') or die(json_encode(array("status" => "unable to write cryptofile", "fileid" => "")));
 	fwrite($fh, $_REQUEST['cryptofile']);
 	fclose($fh);
 
+	//write metadata file
 	$fh = fopen($metadatafile, 'w') or die(json_encode(array("status" => "unable to write metadatafile", "fileid" => "")));
 	fwrite($fh, $_REQUEST['metadata']);
 	fclose($fh);
 
+	// encode json reply
 	echo json_encode(array("status" => "ok", "fileid" => $fileid));
 } else {
 	// THIS IS A NEW REQUEST, SHOW UPLOAD FORM
