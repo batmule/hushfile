ea783fcbbbcd54d69913c4ee016aab5e9326aeec
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-04-17 03:50:07 +0200

    working on server.php

--- server.php
+++ server.php
@@ -1,5 +1,4 @@
 <?php
-
 $datapath = "/usr/local/www/filedata/";
 
 function get_uniqid() {
@@ -11,23 +10,33 @@ function get_uniqid() {
 	return $fileid;
 }
 
-
-if(isset($_REQUEST['cryptofile']) && isset($_REQUEST['metadata'])) {
+if($_SERVER["REQUEST_URI"] != "/") {
+	// THIS IS A FILE DOWNLOAD
+	$fileid = substr($_SERVER["REQUEST_URI"],1);
+	if (file_exists($datapath.$fileid)) {
+		echo "file ready to be downloaded";
+	} else {
+		echo "file not found, expired perhaps?";
+	}
+elseif(isset($_REQUEST['cryptofile']) && isset($_REQUEST['metadata'])) {
+	// THIS IS A FILE UPLOAD
+	
 	// first get a new unique ID for this file
 	$fileid = get_uniqid();
 	$cryptofile = $datapath.$fileid."/cryptofile.dat";
 	$metadatafile = $datapath.$fileid."/metadata.dat";
 	
-	$fh = fopen($cryptofile, 'w') or die("can't open cryptofile");
+	$fh = fopen($cryptofile, 'w') or die(json_encode(array("status" => "unable to write cryptofile", "fileid" => "")));
 	fwrite($fh, $_REQUEST['cryptofile']);
 	fclose($fh);
 
-	$fh = fopen($metadatafile, 'w') or die("can't open metadatafile");
+	$fh = fopen($metadatafile, 'w') or die(json_encode(array("status" => "unable to write metadatafile", "fileid" => "")));
 	fwrite($fh, $_REQUEST['metadata']);
 	fclose($fh);
 
 	echo json_encode(array("status" => "ok", "fileid" => $fileid));
 } else {
-	echo json_encode(array("status" => "fail", "fileid" => ""));
+	// THIS IS A NEW REQUEST, SHOW UPLOAD FORM
+	readfile("client.html");
 }
 ?>
