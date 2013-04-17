cbc601ba911209e8b8ef76186a97a60521f36563
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-04-17 02:48:37 +0200

    fix filenames

--- server.php
+++ server.php
@@ -15,8 +15,8 @@ function get_uniqid() {
 if(isset($_REQUEST['cryptofile']) && isset($_REQUEST['metadata'])) {
 	// first get a new unique ID for this file
 	$fileid = get_uniqid();
-	$cryptofile = $datapath.$fileid."cryptofile";
-	$metadatafile = $datapath.$fileid."metadata";
+	$cryptofile = $datapath.$fileid."/cryptofile.dat";
+	$metadatafile = $datapath.$fileid."/metadata.dat";
 	
 	$fh = fopen($cryptofile, 'w') or die("can't open cryptofile");
 	fwrite($fh, $_REQUEST['cryptofile']);
