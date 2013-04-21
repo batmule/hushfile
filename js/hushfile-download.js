9ec9cacddc6bdaa6cf717afc483154af7367c3a1
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-04-21 23:27:37 +0200

    working on download

--- js/hushfile-download.js
+++ js/hushfile-download.js
@@ -1,50 +1,13 @@
-// from http://freebeer.smithii.com/www/_source.php?file=%2Fhome%2Fross%2Fpublic_html%2Ffreebeer%2Fwww%2Flib%2Fbin2hex.js
-var _hex2bin = [
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, // 0-9
-     0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A-F
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, // a-f
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
-// from http://freebeer.smithii.com/www/_source.php?file=%2Fhome%2Fross%2Fpublic_html%2Ffreebeer%2Fwww%2Flib%2Fbin2hex.js
-function hex2bin(str) {
-	var len = str.length;
-	var rv = '';
-	var i = 0;
-	var c1;
-	var c2;
-	while (len > 1) {
-		h1 = str.charAt(i++);
-		c1 = h1.charCodeAt(0);
-		h2 = str.charAt(i++);
-		c2 = h2.charCodeAt(0);
-		rv += String.fromCharCode((_hex2bin[c1] << 4) + _hex2bin[c2]);
-		len -= 2;
-	}
-	return rv;
-}
-
 function download() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', '/'+fileid+'?filedata', true);
 	xhr.onload = function(e) {
 		if (this.status == 200) {
 			alert("decrypting filedata...");
-			hexfiledata = CryptoJS.AES.decrypt(this.response, password);
-			filedata = hex2bin(hexfiledata.toString());
+			decryptedwords = CryptoJS.AES.decrypt(this.response, password);
+			filedata = CryptoJS.enc.Latin1.stringify(decryptedwords.toString());
 			fileblob = new Blob([filedata], { type: document.getElementById('mimetype').innerHTML });
-			
+
 			// download prompt
 			a = document.createElement("a");
 			a.href = window.URL.createObjectURL(fileblob);
