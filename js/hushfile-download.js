b53a433e2635eb67ff0902d0d492cea8090b4c77
Thomas Steen Rasmussen <thomas@gibfest.dk>
2013-04-27 16:42:07 +0200

    working on download

--- js/hushfile-download.js
+++ js/hushfile-download.js
@@ -1,3 +1,45 @@
+// from http://freebeer.smithii.com/www/_source.php?file=%2Fhome%2Fross%2Fpublic_html%2Ffreebeer%2Fwww%2Flib%2Fbin2hex.js
+function hex2bin(str) {
+    var len = str.length;
+    var rv = '';
+    var i = 0;
+
+    var c1;
+    var c2;
+
+    while (len > 1) {
+        h1 = str.charAt(i++);
+        c1 = h1.charCodeAt(0);
+        h2 = str.charAt(i++);
+        c2 = h2.charCodeAt(0);
+        
+        rv += String.fromCharCode((_hex2bin[c1] << 4) + _hex2bin[c2]);
+        len -= 2;
+    };
+
+    return rv;
+};
+
+// from http://freebeer.smithii.com/www/_source.php?file=%2Fhome%2Fross%2Fpublic_html%2Ffreebeer%2Fwww%2Flib%2Fbin2hex.js
+var _hex2bin = [
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, // 0-9
+     0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A-F
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0,10,11,12,13,14,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, // a-f
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
 function download() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', '/'+fileid+'?filedata', true);
@@ -5,7 +47,7 @@ function download() {
 		if (this.status == 200) {
 			alert("decrypting filedata...");
 			decryptedwords = CryptoJS.AES.decrypt(this.response, password);
-			filedata = CryptoJS.enc.Latin1.stringify(decryptedwords);
+			filedata = hex2bin(CryptoJS.enc.Latin1.stringify(decryptedwords));
 			fileblob = new Blob([filedata], { type: document.getElementById('mimetype').innerHTML });
 
 			// download prompt
